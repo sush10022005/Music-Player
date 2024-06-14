fe1e64715bed5526f56c416b37847dd17b91516e
sush10022005 <kanajsus1010@gmail.com>
2024-06-14 11:30:07 +0530

    new ui

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   const [songs, setSongs] = useState([]);
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef(null);
 
   useEffect(() => {
@@ -15,20 +16,30 @@ function App() {
   const handlePlayPause = () => {
     if (audioRef.current.paused) {
       audioRef.current.play();
+      setIsPlaying(true);
     } else {
       audioRef.current.pause();
+      setIsPlaying(false);
     }
   };
 
   const handleNext = () => {
     setCurrentSongIndex((prevIndex) => (prevIndex + 1) % songs.length);
+    setIsPlaying(false);
   };
 
   const handlePrevious = () => {
     setCurrentSongIndex((prevIndex) =>
       prevIndex === 0 ? songs.length - 1 : prevIndex - 1
     );
+    setIsPlaying(false);
   };
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+    }
+  }, [currentSongIndex]);
 
   return (
     <div className="App">
@@ -37,16 +48,20 @@ function App() {
           <h2>{songs[currentSongIndex].title}</h2>
           <h3>{songs[currentSongIndex].artist}</h3>
           <img src={songs[currentSongIndex].artwork} alt={songs[currentSongIndex].title} />
+          <div className='audio'>
           <audio
             ref={audioRef}
             src={songs[currentSongIndex].url}
             controls
-            autoPlay
           />
+          </div>
+          
           <div className="controls">
-            <button onClick={handlePrevious}>Previous</button>
-            <button onClick={handlePlayPause}>Play/Pause</button>
-            <button onClick={handleNext}>Next</button>
+            <button onClick={handlePrevious}><i className="fas fa-backward"></i></button>
+            <button onClick={handlePlayPause}>
+              {isPlaying ? <i className="fas fa-pause"></i> : <i className="fas fa-play"></i>}
+            </button>
+            <button onClick={handleNext}><i className="fas fa-forward"></i></button>
           </div>
         </div>
       )}
